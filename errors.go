be3e2b7de1a227d548c088578fc6313ddf36295d
Dmitry Yu Okunev <xaionaro@dx.center>
2020-02-09 18:36:53 +0000

    Initial commit

---
+++ errors.go
@@ -0,0 +1,16 @@
+package iotools
+
+type ErrReadFunctionNotDefined struct {}
+func (err *ErrReadFunctionNotDefined) Error() string {
+	return "function Read() is not defined"
+}
+
+type ErrWriteFunctionNotDefined struct {}
+func (err *ErrWriteFunctionNotDefined) Error() string {
+	return "function Write() is not defined"
+}
+
+type ErrCloseFunctionNotDefined struct {}
+func (err *ErrCloseFunctionNotDefined) Error() string {
+	return "function Close() is not defined"
+}
---
+++ new_read_write_closer.go
@@ -0,0 +1,67 @@
+package iotools
+
+import (
+	"io"
+
+	"github.com/xaionaro-go/errors"
+)
+
+type CustomReadWriteCloser struct {
+	ReadFunction func([]byte) (int, error)
+	WriteFunction func([]byte) (int, error)
+	CloseFunction func() error
+}
+
+var _ io.ReadWriteCloser = &CustomReadWriteCloser{}
+
+func NewReadWriteCloser(
+	readFunction func([]byte) (int, error),
+	writeFunction func([]byte) (int, error),
+	closeFunction func() error,
+) *CustomReadWriteCloser {
+	return &CustomReadWriteCloser{
+		ReadFunction:  readFunction,
+		WriteFunction: writeFunction,
+		CloseFunction: closeFunction,
+	}
+}
+
+func (rwc *CustomReadWriteCloser) Read(b []byte) (n int, err error) {
+	defer func() { err = errors.Wrap(err) }()
+	if rwc.ReadFunction == nil {
+		return 0, &ErrReadFunctionNotDefined{}
+	}
+	return rwc.ReadFunction(b)
+}
+
+func (rwc *CustomReadWriteCloser) Write(b []byte) (n int, err error) {
+	defer func() { err = errors.Wrap(err) }()
+	if rwc.ReadFunction == nil {
+		return 0, &ErrWriteFunctionNotDefined{}
+	}
+	return rwc.WriteFunction(b)
+}
+
+func (rwc *CustomReadWriteCloser) Close() (err error) {
+	defer func() { err = errors.Wrap(err) }()
+	if rwc.ReadFunction == nil {
+		return &ErrCloseFunctionNotDefined{}
+	}
+	return rwc.CloseFunction()
+}
+
+func NewWriter(writeFunction func([]byte) (int, error)) *CustomReadWriteCloser {
+	return &CustomReadWriteCloser{
+		WriteFunction: writeFunction,
+	}
+}
+
+func NewWriteCloser(
+	writeFunction func([]byte) (int, error),
+	closeFunction func() error,
+) *CustomReadWriteCloser {
+	return &CustomReadWriteCloser{
+		WriteFunction: writeFunction,
+		CloseFunction: closeFunction,
+	}
+}
